f36c522a53589894e14a2320eac2861565e19401
tidaa <168005374+tidaa@users.noreply.github.com>
2024-06-19 16:24:24 +0200

    tida

--- js/ylli.js
+++ js/ylli.js
@@ -32,3 +32,6 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+document.querySelector('.nav-toggle-btn').addEventListener('click', function() {
+  document.querySelector('.navbar').classList.toggle('open');
+});
